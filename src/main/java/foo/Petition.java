29e1a42a514d7596578baf68b248e870a2b9135c
nicolas <nicolas.stucky@etu.univ-nantes.fr>
2023-12-21 19:07:49 +0100

    Refactor PetitionEndPoint and Creation_Petiton.html

---
+++ src/main/java/foo/Petition.java
@@ -0,0 +1,8 @@
+package foo;
+
+public class Petition {
+    public String owner;
+	public String title;
+	public String description;
+    public String tag;
+}
--- src/main/java/foo/PetitionEndPoint.java
+++ src/main/java/foo/PetitionEndPoint.java
@@ -1,23 +1,40 @@
 package foo;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
 import com.google.api.server.spi.auth.common.User;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiMethod.HttpMethod;
 import com.google.api.server.spi.config.ApiNamespace;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.config.Nullable;
+import com.google.api.server.spi.response.CollectionResponse;
+import com.google.api.server.spi.response.UnauthorizedException;
+import com.google.api.server.spi.auth.EspAuthenticator;
 
+import com.google.appengine.api.datastore.Cursor;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.PropertyProjection;
+import com.google.appengine.api.datastore.PreparedQuery.TooManyResultsException;
+import com.google.appengine.api.datastore.Query.CompositeFilter;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.FilterPredicate;
-
-import java.io.IOException;
-import java.util.Date;
-import java.util.List;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Transaction;
 
 @Api(name = "myApi",
      version = "v1",
@@ -31,24 +48,24 @@ import java.util.List;
 		   packagePath = "")
      )
 
-public class PetitionEndpoint {
+public class ScoreEndpoint {
 
-        @ApiMethod(name = "createPetition")
-        public Entity createPetition(User user, String title, String description, String tags) {
-        
-            if (user == null) {
-                return 510;
-            }
+    @ApiMethod(name = "createPetition", httpMethod = HttpMethod.POST)
+    public Entity createPetition(Petition petition) {
+        // Utilise les propriétés de l'objet Petition pour créer une entité et la stocker dans le Datastore
+        Entity e = new Entity("Petition" + petition.getTitle());
+        e.setProperty("title", petition.getTitle());
+        e.setProperty("description", petition.getDescription());
+        e.setProperty("tags", petition.getTags());
+        pertitionEntity.setProperty("owner", petition.owner);
+        e.setProperty("signature", 0);
+		e.setProperty("date", new Date());
 
-            org.w3c.dom.Entity petitionEntity = new Entity(title + "Petition");
-            petitionEntity.setProperty("title", title);
-            petitionEntity.setProperty("description", description);
-            petitionEntity.setProperty("tags", tags);
-            petitionEntity.setPrefix("owner", user.getEmail());
-    
-            DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-            datastore.put(petitionEntity);
-    
-            return petitionEntity;
-        }
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        Transaction txn = datastore.beginTransaction();
+        datastore.put(e);
+        txn.commit();
+        return e;
     }
+}
+
